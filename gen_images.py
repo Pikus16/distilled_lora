f499207fbc2c3da125759128055c08ce6cc15db8
Benjamin Pikus <ben_pikus@Benjamins-MacBook-Pro-3.local>
2025-03-11 09:17:26 -0700

    add pretrained generation

--- gen_images.py
+++ gen_images.py
@@ -20,39 +20,50 @@ def load_pipe(model_name, lora_path):
         torch_dtype=torch.float16,
         use_safetensors=True,
     ).to("cuda")
-    pipe_.load_lora_weights(lora_path)
+    if lora_path is not None:
+        pipe_.load_lora_weights(lora_path)
     pipe_.set_progress_bar_config(disable=True)
     diffusers.logging.set_verbosity_error()
     return pipe_
 
-def generate_images(pipe, fast_pipe, prompt_list, dst_dir, n_steps_list, num_images_per_prompt):
+def generate_images(normal_pipe, pipe, fast_pipe, prompt_list, dst_dir, n_steps_list, num_images_per_prompt):
     os.makedirs(dst_dir, exist_ok=True)
-    # generate standard images
-    regular_dir = f'{dst_dir}/regular'
-    os.makedirs(regular_dir, exist_ok=True)
-    ct = 0
-    for prompt in tqdm(prompt_list):
-        images = pipe(prompt, generator=torch.Generator(device="cuda").manual_seed(0), num_images_per_prompt=num_images_per_prompt).images
-        for img in images:
-            img.save(f'{regular_dir}/img_{ct}.png')
-            ct += 1
 
-    # generate fast images
-    for n_steps in n_steps_list:
-        fast_dir = f'{dst_dir}/fast_{n_steps}'
-        os.makedirs(fast_dir, exist_ok=True)
+    if normal_pipe is not None:
+        normal_dir = f'{dst_dir}/pretrained'
+        os.makedirs(normal_dir, exist_ok=True)
+        for i, prompt in tqdm(enumerate(prompt_list), total=len(prompt_list)):
+            images = normal_pipe(prompt, generator=torch.Generator(device="cuda").manual_seed(0)).images
+            images[0].save(f'{normal_dir}/img_{i}.png')
+
+    if pipe is not None:
+        # generate standard images
+        regular_dir = f'{dst_dir}/regular'
+        os.makedirs(regular_dir, exist_ok=True)
         ct = 0
         for prompt in tqdm(prompt_list):
-            images = fast_pipe(prompt=prompt, num_inference_steps=n_steps, guidance_scale=0.0, generator=torch.Generator(device="cuda").manual_seed(0)).images
+            images = pipe(prompt, generator=torch.Generator(device="cuda").manual_seed(0), num_images_per_prompt=num_images_per_prompt).images
             for img in images:
-                img.save(f'{fast_dir}/img_{ct}.png')
+                img.save(f'{regular_dir}/img_{ct}.png')
                 ct += 1
+
+    if fast_pipe is not None:
+        # generate fast images
+        for n_steps in n_steps_list:
+            fast_dir = f'{dst_dir}/fast_{n_steps}'
+            os.makedirs(fast_dir, exist_ok=True)
+            ct = 0
+            for prompt in tqdm(prompt_list):
+                images = fast_pipe(prompt=prompt, num_inference_steps=n_steps, guidance_scale=0.0, generator=torch.Generator(device="cuda").manual_seed(0)).images
+                for img in images:
+                    img.save(f'{fast_dir}/img_{ct}.png')
+                    ct += 1
     
-def clear_pipes(pipe, fast_pipe):
-    pipe.to('cpu')
-    fast_pipe.to('cpu')
-    del pipe
-    del fast_pipe
+def clear_pipes(pipe_list):
+    for pipe in pipe_list:
+        if pipe is not None:
+            pipe.to('cpu')
+            del pipe
     torch.cuda.empty_cache()
 
 @click.command()
@@ -85,8 +96,9 @@ def main(lora_models, n_steps, num_images_per_prompt):
         
         pipe = load_pipe('stabilityai/stable-diffusion-xl-base-1.0', lora_path)
         fast_pipe = load_pipe('stabilityai/sdxl-turbo', lora_path)
-        generate_images(pipe, fast_pipe, prompts, f'images/{lora_base}', n_steps, num_images_per_prompt)
-        clear_pipes(pipe, fast_pipe)
+        normal_pipe = load_pipe('stabilityai/stable-diffusion-xl-base-1.0', lora_path=None)
+        generate_images(normal_pipe, pipe, fast_pipe, prompts, f'images/{lora_base}', n_steps, num_images_per_prompt)
+        clear_pipes([pipe, fast_pipe, normal_pipe])
 
 if __name__ == '__main__':
     main()
